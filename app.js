3a17881001be4717db8762c118295f53c0bee938
fabpink <69956772+fabpink@users.noreply.github.com>
2020-08-20 15:25:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCy6Q6105CLOUC2j3AkQEiHlKV8wTuZKEk",
+  authDomain: "voting2020-35113.firebaseapp.com",
+  databaseURL: "https://voting2020-35113.firebaseio.com",
+  projectId: "voting2020-35113",
+  storageBucket: "voting2020-35113.appspot.com",
+  messagingSenderId: "926291759405",
+  appId: "1:926291759405:web:8c18f06b1736a7696103a4",
+  measurementId: "G-6L46DWQJKS"
 };
 
      
